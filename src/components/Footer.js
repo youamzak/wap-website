10f18f48c52fc026e84aee2f65d29756541974f5
Zakaria Chegdali <zchegdali@gmail.com>
2022-07-13 17:04:13 +0200

    footer infos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,23 @@ import styled from 'styled-components'
 
 const FooterStyled = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
   margin-right: auto;
   margin-left: auto;
   margin-top: 10px;
-  width: 80%;
+  width: 100%;
   height: 100px;
+  p {
+    margin-top: 0;
+  }
 `
 const Footer = () => {
   return (
     <FooterStyled className="bk_red color_light" >
-      Footer
+      <p>© - Web Automation Project 2022 Tous droits réservés</p>
+      <p> zchegdali@gmail.com - +33(0)6.XX.XX.XX.XX </p>
     </FooterStyled>
   )
 }
